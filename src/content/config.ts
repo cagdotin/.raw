b7ba5ed1bad23a197aef7711891fe6a1a5cb3368
0xcgn <13513097+0xcgn@users.noreply.github.com>
2024-10-12 21:04:36 +0200

    update post content schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,30 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z
+      .string()
+      .max(
+        60,
+        "Title should be 60 or less chars for optimal OpenGraph display."
+      ),
+    description: z
+      .string()
+      .max(
+        155,
+        "Description should be 155 or less chars for optimal OpenGraph display."
+      ),
+    tags: z
+      .array(z.string())
+      .max(3, "Too many tags (3+), less is more my friend. Tag wisely!")
+      .optional(),
+    authors: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+    publishedAt: z.coerce.date(),
+    updatedAt: z.coerce.date().optional(),
+    image: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
