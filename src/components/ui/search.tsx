67eba303a8c58bfd07326270f8ef0ee371f74d4f
0xcgn <13513097+0xcgn@users.noreply.github.com>
2024-10-13 14:27:13 +0200

    add search placeholder

---
+++ src/components/ui/search.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+// TODO: update this placeholder.
+export const Search = () => {
+  return (
+    <Button
+      variant="ghost"
+      size="sm"
+      className={cn(
+        "relative border-x w-full justify-start text-xs font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 grow"
+      )}
+      //   onClick={() => setOpen(true)}
+      //   {...props}
+    >
+      <span className="inline-flex">Search...</span>
+      <kbd className="pointer-events-none absolute right-[0.5rem] top-[0.5rem] hidden h-5 select-none items-center gap-1 border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+        <span className="text-xs">⌘</span>K
+      </kbd>
+    </Button>
+  );
+};
