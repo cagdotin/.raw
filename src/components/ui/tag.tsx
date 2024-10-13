4893ad1a20820e1db57389f4b545ef56e6bbde41
0xcgn <13513097+0xcgn@users.noreply.github.com>
2024-10-13 14:58:38 +0200

    fix responsive tags issue

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -6,7 +6,10 @@ interface Props {
 
 export const Tag = ({ tag }: Props) => {
   return (
-    <Badge variant="secondary" className="rounded-sm px-1 font-normal text-xs">
+    <Badge
+      variant="secondary"
+      className="rounded-sm px-1 font-normal text-xs mt-1 mr-1"
+    >
       #{tag}
     </Badge>
   );
