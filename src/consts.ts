15af7459fbdd1a2c371988df222bd14d39aaf84c
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-10-18 13:18:01 +0200

    added main nav to site_config

--- src/consts.ts
+++ src/consts.ts
@@ -1,18 +1,19 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-import { getEntry } from "astro:content";
-
-const author = await getEntry("authors", "cagin");
-const { socials } = author.data;
-
 export const site_config = {
   title: ".raw",
   description:
-    "Astro.js template in brutalist style, focusing on raw simplicity and functionality. Minimalist design for developers who appreciate the bare essentials.",
+    ".raw is an opinionated static blogging template built with Astro, Tailwind , and shadcn/ui.",
   socials: {
-    twitter: socials.twitter,
-    github: socials.github,
+    twitter: "https://x.com/cagdotin",
+    github: "https://github.com/cagdotin/.raw",
     rss: "https://example.com/rss",
   },
+  main_nav: [
+    { title: "home", href: "/" },
+    { title: "about", href: "/about" },
+    { title: "blog", href: "/blog" },
+    { title: "components", href: "/blog/components" },
+  ],
 };
