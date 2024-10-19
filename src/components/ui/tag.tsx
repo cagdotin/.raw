4796938d9f8e7b39986d38d3a56736346e715f8e
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-10-19 20:21:13 +0200

    fix broken mdx

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -1,14 +1,19 @@
 import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 interface Props {
   tag: string;
+  className?: string;
 }
 
-export const Tag = ({ tag }: Props) => {
+export const Tag = ({ tag, className }: Props) => {
   return (
     <Badge
       variant="secondary"
-      className="rounded-sm px-1 font-normal text-xs mt-1 mr-1"
+      className={cn([
+        "rounded-sm px-1 font-normal text-xs mt-1 mr-1",
+        className,
+      ])}
     >
       #{tag}
     </Badge>
